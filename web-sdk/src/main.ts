63d1b519dd4a182bb5e22537721051eab4b28035
sourour9 <sourour.gazzeh@outlook.fr>
2023-10-12 23:20:06 +0200

    fix

--- web-sdk/src/main.ts
+++ web-sdk/src/main.ts
@@ -10,19 +10,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // Retrieve config service
   const configService = app.get(ConfigService);
 
-  // Add validation pipe for all endpoints
   app.useGlobalPipes(new ValidationPipe());
 
-  // Servir les fichiers statiques
   app.use(express.static(join(__dirname, '..', 'public')));
 
-  // Starts listening for shutdown hooks
   app.enableShutdownHooks();
 
-  // Run the app
   const appPort = configService.get('app.port');
   console.log(appPort);
   await app.listen(appPort);
