895546951bab642f6fd680a11931fdb989950d35
sourour9 <sourour.gazzeh@outlook.fr>
2023-10-14 12:49:18 +0200

    use rsa to encrypt card information #3

--- web-sdk/src/sdk/controllers/payment.controller.ts
+++ web-sdk/src/sdk/controllers/payment.controller.ts
@@ -1,29 +1,41 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import * as jwt from 'jsonwebtoken';
+import * as crypto from 'crypto';
 
-  @Controller('payment')
-  export class PaymentController {
-    @Post()
-    processPayment(@Body() cardInfo: any) {
-
-      try {
-        if (!cardInfo || !cardInfo.cardNumber || !cardInfo.expirationDate || !cardInfo.cvv) {
-           throw new Error('Invalid card information');
-        }
-        const payload = {
-          cardNumber: cardInfo.cardNumber,
-          expirationDate: cardInfo.expirationDate,
-          cvv: cardInfo.cvv,
-        };
-        const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1800s' }); // 1800s = 30min
-        //envoyer au gateway
-        console.debug('Received Card Information:', cardInfo);
-        console.debug('Generated Token:', token);
-        return { message: 'Payment received successfully!' };
-      } catch (error) {
-        console.error('Error processing payment:', error);
-        return { message: 'Error processing payment' };
+@Controller('payment')
+export class PaymentController {
+  @Post()
+  processPayment(@Body() cardInfo: any) {
+    try {
+      if (!cardInfo || !cardInfo.cardNumber || !cardInfo.expirationDate || !cardInfo.cvv) {
+        throw new Error('Invalid card information');
       }
-    }
 
+      const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+          type: 'spki',
+          format: 'pem',
+        },
+        privateKeyEncoding: {
+          type: 'pkcs8',
+          format: 'pem',
+        },
+      });
+
+      const encryptedCardInfo = crypto.publicEncrypt(publicKey, Buffer.from(JSON.stringify(cardInfo)));
+
+      console.debug('Encrypted Card Information:', encryptedCardInfo.toString('base64'));
+
+      const decryptedCardInfo = crypto.privateDecrypt(privateKey, encryptedCardInfo);
+
+      const parsedCardInfo = JSON.parse(decryptedCardInfo.toString());
+      console.debug('Decrypted Card Information:', parsedCardInfo);
+
+      return { message: 'Payment received successfully!' };
+    } catch (error) {
+      console.error('Error processing payment:', error);
+      return { message: 'Error processing payment' };
+    }
+  }
 }
+
