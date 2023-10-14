ab99727e67a8051450029c55c7f1491d51cc39f5
sourour9 <sourour.gazzeh@outlook.fr>
2023-10-14 13:56:04 +0200

    retrieve location information #5

--- web-sdk/src/sdk/controllers/payment.controller.ts
+++ web-sdk/src/sdk/controllers/payment.controller.ts
@@ -1,14 +1,28 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import * as crypto from 'crypto';
-
+import axios from 'axios';
 @Controller('payment')
 export class PaymentController {
   @Post()
-  processPayment(@Body() cardInfo: any) {
+  async processPayment(@Body() cardInfo: any) {
     try {
       if (!cardInfo || !cardInfo.cardNumber || !cardInfo.expirationDate || !cardInfo.cvv) {
         throw new Error('Invalid card information');
       }
+      const ipInfoResponse = await axios.get('https://ipinfo.io');
+      const location = ipInfoResponse.data.loc;
+      if (!location) {
+              throw new Error('Unable to retrieve location information');
+      }
+      console.debug('localisation:', location);
+      const [altitude, longitude] = location.split(',');
+      const payload = {
+              cardNumber: cardInfo.cardNumber,
+              expirationDate: cardInfo.expirationDate,
+              cvv: cardInfo.cvv,
+              altitude,
+              longitude,
+            };
 
       const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
         modulusLength: 2048,
@@ -21,16 +35,11 @@ export class PaymentController {
           format: 'pem',
         },
       });
-
-      const encryptedCardInfo = crypto.publicEncrypt(publicKey, Buffer.from(JSON.stringify(cardInfo)));
-
+      const encryptedCardInfo = crypto.publicEncrypt(publicKey, Buffer.from(JSON.stringify(payload)));
       console.debug('Encrypted Card Information:', encryptedCardInfo.toString('base64'));
-
       const decryptedCardInfo = crypto.privateDecrypt(privateKey, encryptedCardInfo);
-
       const parsedCardInfo = JSON.parse(decryptedCardInfo.toString());
       console.debug('Decrypted Card Information:', parsedCardInfo);
-
       return { message: 'Payment received successfully!' };
     } catch (error) {
       console.error('Error processing payment:', error);
