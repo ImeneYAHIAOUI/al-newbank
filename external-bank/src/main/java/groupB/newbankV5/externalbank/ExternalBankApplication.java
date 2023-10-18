226961f9c0993ffc26b7ce6fca52b92d08ff3451
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2023-10-18 23:35:04 +0200

    push

---
+++ external-bank/src/main/java/groupB/newbankV5/externalbank/ExternalBankApplication.java
@@ -0,0 +1,13 @@
+package groupB.newbankV5.externalbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalBankApplication.class, args);
+    }
+
+}
