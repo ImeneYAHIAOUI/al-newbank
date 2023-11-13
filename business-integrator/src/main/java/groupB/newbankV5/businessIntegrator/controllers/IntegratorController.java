51ca23f2e7c948180b39013f83681e6b314afa5f
nbenaissa <nadim.ben-aissa@etu.univ-cotedazur.fr>
2023-11-13 17:53:54 +0100

    delete deplicated method

--- business-integrator/src/main/java/groupB/newbankV5/businessIntegrator/controllers/IntegratorController.java
+++ business-integrator/src/main/java/groupB/newbankV5/businessIntegrator/controllers/IntegratorController.java
@@ -57,11 +57,6 @@ public class IntegratorController {
         Merchant merchant = businessFinder.findMerchantById(id);
         return ResponseEntity.ok().body(merchant);
     }
-    @GetMapping("/merchants")
-    public ResponseEntity<BankAccount> getMerchantBankAccount(@RequestParam("name") String name) throws MerchantNotFoundException {
-        log.info("Getting merchant " + name);
-        return ResponseEntity.ok().body(businessFinder.findMerchantAccountByName(name));
-    }
 
     @GetMapping("/merchants")
     public ResponseEntity<BankAccount> getMerchantBankAccount(@RequestParam("name") String name) throws MerchantNotFoundException {
