3e642cbd1b4895e7ed9477497f2cd014cb3ae9c6
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2023-11-22 12:58:01 +0100

    fix

--- start-all.sh
+++ start-all.sh
@@ -7,13 +7,14 @@ services=(
     "payment-gateway:payment-gateway/docker-compose.yml"
     "customer-care:customer-care/docker-compose.yml"
     "fees-calculator:fees-calculator/docker-compose.yml"
-    "external-bank:external-bank/docker-compose.yml"
     "mock-credit-card-network:mock-credit-card-network/docker-compose.yml"
     "business-integrator:business-integrator/docker-compose.yml"
     "payment-settlement:payment-settlement/docker-compose.yml"
     "payment-processor:payment-processor/docker-compose.yml"
     "transactions-service:transactions-service/docker-compose.yml"
     "analytics-service:analytics-service/docker-compose.yml"
+    "external-bank:external-bank/docker-compose.yml"
+
 )
 
 container_ids=()
