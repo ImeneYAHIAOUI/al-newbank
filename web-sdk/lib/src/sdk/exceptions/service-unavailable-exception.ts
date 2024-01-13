ba504634a231ed936f91e15b61832f0d6369a4d8
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2024-01-13 14:02:05 +0100

    Update service-unavailable-exception.ts

--- web-sdk/lib/src/sdk/exceptions/service-unavailable-exception.ts
+++ web-sdk/lib/src/sdk/exceptions/service-unavailable-exception.ts
@@ -1,13 +1,8 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
-export class ServiceUnavailableException extends HttpException {
+export class ServiceUnavailableException extends Error {
   constructor(message: string = 'Service Unavailable', retryAfter?: number) {
-    const headers: Record<string, string> = {};
-    
-    if (retryAfter) {
-      headers['Retry-After'] = retryAfter.toString();
-    }
-
-    super({ message, headers }, HttpStatus.SERVICE_UNAVAILABLE);
+    super(`${message} ${retryAfter}`);
+    this.name = 'ServiceUnavailable';
   }
 }
