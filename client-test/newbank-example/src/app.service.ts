9e44f14939768562e53dce05045c0afc5f039fda
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2024-01-13 13:38:19 +0100

    fix in logs

--- client-test/newbank-example/src/app.service.ts
+++ client-test/newbank-example/src/app.service.ts
@@ -22,7 +22,7 @@ export class AppService {
         await this.newbankSdk.confirmPayment(result.transactionId);
 
     }catch(error : any){
-        console.log(error)
+        console.log(error.response.message + error.response.headers);
         const start = new Date().getTime();
         const delayMilliseconds = 2000; 
         while (new Date().getTime() - start < delayMilliseconds) {
