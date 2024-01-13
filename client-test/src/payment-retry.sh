aa96c30f684c6ca7870733ed6b996f0420ec9d97
BEN AISSA Nadim <nadim.ben-aissa@etu.univ-cotedazur.fr>
2024-01-13 11:26:44 +0100

    add payment-retry

---
+++ client-test/src/payment-retry.sh
@@ -0,0 +1,58 @@
+# Assuming .env is in the same directory as your script
+source .env
+
+# Alternatively, you can use the shorthand notation:
+# . .env
+
+# Extract values from environment variables
+cardNumber=$CARD_NUMBER
+cvv=$CVV
+expiryDate=$EXPIRY_DATE
+
+# Print the extracted values for verification
+echo "Card Number: $cardNumber"
+echo "CVV: $cvv"
+echo "Expiry Date: $expiryDate"
+
+paymentDto='{
+     "cardNumber": "'"${cardNumber}"'",
+     "cvv": "'"${cvv}"'",
+     "expirationDate": "'"${expiryDate}"'",
+     "amount": 500
+}'
+
+
+url1="http://localhost:3501/api/gateway_authorization/simulate?errorCode="
+url="http://localhost:3503/api/gateway_authorization/simulate?errorCode="
+url2="http://localhost:5001/api/timeout"
+
+ERROR_CODE=200
+
+response=$(curl -s -X POST "${url}${ERROR_CODE}" -H "Content-Type: application/json" -d '{}')
+response=$(curl -s -X POST "${url1}${ERROR_CODE}" -H "Content-Type: application/json" -d '{}')
+response=$(curl -s -X POST "${url2}" -H "Content-Type: application/json" -d '{}')
+
+curl -s -X POST -H "Content-Type: application/json" -d "$paymentDto" "http://localhost:6906/pay"
+
+sleep 2
+
+
+url1="http://localhost:3501/api/gateway_authorization/simulate?errorCode="
+url="http://localhost:3503/api/gateway_authorization/simulate?errorCode="
+
+ERROR_CODE=500
+
+response=$(curl -s -X POST "${url}${ERROR_CODE}" -H "Content-Type: application/json" -d '{}')
+response=$(curl -s -X POST "${url1}${ERROR_CODE}" -H "Content-Type: application/json" -d '{}')
+
+curl -s -X POST -H "Content-Type: application/json" -d "$paymentDto" "http://localhost:6906/pay"
+
+sleep 1
+
+url1="http://localhost:3501/api/gateway_authorization/simulate?errorCode="
+url="http://localhost:3503/api/gateway_authorization/simulate?errorCode="
+
+ERROR_CODE=200
+
+response=$(curl -s -X POST "${url}${ERROR_CODE}" -H "Content-Type: application/json" -d '{}')
+response=$(curl -s -X POST "${url1}${ERROR_CODE}" -H "Content-Type: application/json" -d '{}')
