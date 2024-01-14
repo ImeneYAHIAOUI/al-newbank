dd5c4185e0f55014066f9c49a8e46823f475d79f
BEN AISSA Nadim <nadim.ben-aissa@etu.univ-cotedazur.fr>
2024-01-14 17:46:33 +0100

    update sdk

--- web-sdk/src/sdk/exceptions/service-unavailable-exception.ts
+++ web-sdk/src/sdk/exceptions/service-unavailable-exception.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class ServiceUnavailableException extends Error {
   constructor(message: string = 'Service Unavailable', retryAfter?: number) {
-    super(`${message}. Retry after ${retryAfter}`);
+    super(`${message}. Retry after ${retryAfter}s`);
     this.name = 'ServiceUnavailable';
   }
 }
