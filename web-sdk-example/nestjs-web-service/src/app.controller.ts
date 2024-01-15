82e9a9709a8ceb1e9b6b0a5c4aac07059ac26d64
BEN AISSA Nadim <nadim.ben-aissa@etu.univ-cotedazur.fr>
2024-01-15 11:13:48 +0100

    Update app.controller.ts

--- web-sdk-example/nestjs-web-service/src/app.controller.ts
+++ web-sdk-example/nestjs-web-service/src/app.controller.ts
@@ -23,7 +23,7 @@ export class AppController {
 
     @Post('/pay')
     async payment(@Body() paymentInfoDTO: PaymentInfoDTO): Promise<void> {
-      this.appService.pay(paymentInfoDTO);
+      this.appService.payment(paymentInfoDTO);
     }
 
     @Get('/backend-status')
